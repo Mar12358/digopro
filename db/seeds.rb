0fd1efd3b1da480131f2e161cdb7a7962071fd18
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-22 05:23:07 -0300

    Fix seed image link

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ users = [
 lectures = [
   {
   "name": "Lindy Hop",
-  "image_url": "https://github.com/Mar12358/book-an-appointment/tree/features/app/assets/images/lecture_lindy_hop.png",
+  "image_url": "https://raw.githubusercontent.com/Mar12358/book-an-appointment/202464a0370bb50779d9396bfa88028e2142be22/app/assets/images/lecture_lindy_hop.png",
   "description": "Learn Swing Jazz Dance with, with the roots of dance improvisazion with a partner",
   "fb_link": "https://swingcity.com.ar",
   "tw_link": "https://twitter.com/home",
