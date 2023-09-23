a0ccc8194cf54ffdd39b22f904986c7532827f06
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-09-23 13:19:29 -0300

    React render running

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,24 +1,15 @@
+// Entry point for the build script in your package.json
 import '@hotwired/turbo-rails';
 import './controllers';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import store from './redux/store';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<p> Hello, Chris! </p>} />
+    </Routes>
   );
 }
 
-ReactDOM.createRoot(
-  <App />,
-  document.getElementById('root'),
-);
+export default App;
