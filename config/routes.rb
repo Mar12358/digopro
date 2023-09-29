8da805a0867c0a4b2f290f07fe20da6de0a1a7e8
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-28 22:20:08 -0600

    Adding destroy to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :lectures, only: [:index, :show, :create]
+      resources :lectures, only: [:index, :show, :create, :destroy]
       resources :users, only: [:index, :show] do
         resources :reservations, only: [:index, :create]
       end
