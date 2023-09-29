4ebc4928ef1d3f1044a9dd178de5fda10e20a74f
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-28 22:29:08 -0600

    Implement greetings Component to delete lectures

---
+++ app/javascript/pages/Greetings.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMessages } from '../redux/messages/messagesSlice';
+import Greeting from '../components/Greeting';
+import './Greetings.css'
+
+export default function Greetings() {
+  const dispatch = useDispatch();
+
+  // Access the 'messageList' and 'status' properties from the Redux store
+  const { messageList, status } = useSelector((store) => store.message);
+   
+  useEffect(() => {
+    // Check the status from the Redux store
+    if (status === 'idle') {
+      dispatch(fetchMessages());
+    }
+  }, [status, dispatch]);
+
+  const myMessages = [];
+
+  for (let i = 0; i < messageList.length; i += 1) {
+    const str = `message${i}`;
+    myMessages.push(<Greeting
+      key={str}
+      id={messageList[i].id}
+      name={messageList[i].name}
+      image_url={messageList[i].image_url}
+      description={messageList[i].description}
+      web_link={messageList[i].web_link}
+      price={messageList[i].price} 
+    />);
+  }
+
+
+
+  return (
+    <div className="container">
+      {myMessages}
+    </div>
+  );
+}
