d6dd7aef8d8266d88ff3789dcb78f936ffc96b79
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-28 22:27:23 -0600

    Add Message reduce to store

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reservationReducer from './reservation/reservationReducer';
 import lectureReducer from './lecture/lectureReducer';
+import messageReducer from './messages/messagesSlice';
 
 const store = configureStore({
   reducer: {
     reservation: reservationReducer,
     lecture: lectureReducer,
+    message: messageReducer,
   },
 });
 
