bef42dd678280413e906a496153df01db86fc64c
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-09-30 19:04:45 +0100

    mobile higest

--- app/javascript/components/Lecture.js
+++ app/javascript/components/Lecture.js
@@ -76,7 +76,7 @@ const Lecture = () => {
             </button>
           </div>
 
-          <div className="grid md:grid-cols-3 md:grid-rows-3 gap-2 w-[100%] md:h-[35rem] grid-cols-1  grid-row-2 justify-items-center">
+          <div className="grid md:grid-cols-3 md:grid-rows-3 gap-2 w-[100%] md:h-[35rem] h-full grid-cols-1  grid-row-2 justify-items-center">
             {displayedLectures.map((lecture) => (
               <div
                 key={lecture.id}
