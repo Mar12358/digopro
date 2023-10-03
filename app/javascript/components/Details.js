527791dfc03a18f1cbf2d0ff6688d39bea0ddfce
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-10-03 20:13:43 -0300

    Destructureing links and id for lecture

--- app/javascript/components/Details.js
+++ app/javascript/components/Details.js
@@ -3,9 +3,6 @@ import './Details.css';
 import { NavLink } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
-import configIcon from '../images/config.png';
-import nextIcon from '../images/next_white.png';
-import backIcon from '../images/back.png';
 import discoverIcon from '../images/discover.png';
 import arrowRight from '../images/arrow_right.png';
 
@@ -13,16 +10,13 @@ function Details() {
   const { currentLectureId } = useSelector((store) => store.currentLecture);
   const { allLecture } = useSelector((state) => state.lecture);
 
-  const { id } = currentLectureId;
-
-  const currentLecture = allLecture.filter((lecture) => lecture.id === id);
-
+  const currentLecture = allLecture.filter((lecture) => lecture.id === currentLectureId);
   const { description } = currentLecture[0];
-  const { imageUrl } = currentLecture[0];
+  const { image_url: imageUrl } = currentLecture[0];
   const { name } = currentLecture[0];
   const { price } = currentLecture[0];
-  const { webLink } = currentLecture[0];
-
+  const { web_link: webLink } = currentLecture[0];
+  const { id } = currentLecture[0];
   return (
 
     <div className="container-main-details">
@@ -50,7 +44,7 @@ function Details() {
           </div>
 
           <div className="web-link">
-            <a href={webLink} target="_blank" rel="noreferrer">web link</a>
+            <a href={webLink} target="_blank" rel="noreferrer">Visit our Web</a>
           </div>
 
           <div className="discover-more">
@@ -61,23 +55,13 @@ function Details() {
           <img src={discoverIcon} className="discover-icon" alt="discover icon" />
 
           <div className="next-button">
-            <img src={configIcon} className="config-icon" alt="config icon" />
-            <div className="color-white">Configure</div>
-
             <NavLink to="/reserve" className="color-white">
               Reserve
             </NavLink>
-
-            <img src={nextIcon} className="next-icon" alt="next icon" />
           </div>
         </div>
 
       </div>
-
-      <NavLink to="/lectures" className="back-button">
-        <img src={backIcon} className="back-icon" alt="back icon" />
-      </NavLink>
-
     </div>
   );
 }
