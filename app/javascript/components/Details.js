31d0858ee5ffd1c0ad4b06b415650029df8b9538
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-10-05 14:44:54 -0300

    Fix calling images

--- app/javascript/components/Details.js
+++ app/javascript/components/Details.js
@@ -4,11 +4,11 @@ import { NavLink } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 
-import configIcon from '../images/config.png';
-import nextIcon from '../images/next_white.png';
-import backIcon from '../images/back.png';
-import discoverIcon from '../images/discover.png';
-import arrowRight from '../images/arrow_right.png';
+import configIcon from '../assets/images/config.png';
+import nextIcon from '../assets/images/next_white.png';
+import backIcon from '../assets/images/back.png';
+import discoverIcon from '../assets/images/discover.png';
+import arrowRight from '../assets/images/arrow_right.png';
 
 const Details = () => {
   const { currentLectureId } = useSelector((store) => store.currentLecture);
