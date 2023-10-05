978efe97cd07d46d48676cdcdeeffb8f6fd08f9d
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-10-05 02:59:46 -0600

    Fix linters

--- app/javascript/components/Details.js
+++ app/javascript/components/Details.js
@@ -4,12 +4,11 @@ import { NavLink } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 
-import configIcon from '../images/config.png'
-import nextIcon from '../images/next_white.png'
-import backIcon from '../images/back.png'
-import discoverIcon from '../images/discover.png'
-import arrowRight from '../images/arrow_right.png'
-
+import configIcon from '../images/config.png';
+import nextIcon from '../images/next_white.png';
+import backIcon from '../images/back.png';
+import discoverIcon from '../images/discover.png';
+import arrowRight from '../images/arrow_right.png';
 
 const Details = () => {
   const { currentLectureId } = useSelector((store) => store.currentLecture);
@@ -23,59 +22,61 @@ const Details = () => {
   const { web_link: webLink } = currentLecture[0];
   const { id } = currentLecture[0];
   return (
-      <div className="container-details">
-        <img src={imageUrl} className="imageUrl img-big" alt="next icon" />
-        <div className="container-extra-1">
-          <div className="card">
+    <div className="container-details">
+      <img src={imageUrl} className="imageUrl img-big" alt="next icon" />
+      <div className="container-extra-1">
+        <div className="card">
 
-            <div className="container-extra-2">
-              <img src={imageUrl} className="imageUrl img-small" alt="next icon" /> 
-              
-              <div className="container-extra-3">
-                <div className="title">{name} </div>
-                <p>id {id}</p>
-                <div className="description">
-                  {description}
-                </div>
+          <div className="container-extra-2">
+            <img src={imageUrl} className="imageUrl img-small" alt="next icon" />
+
+            <div className="container-extra-3">
+              <div className="title">
+                {name}
+                {' '}
+              </div>
+              <p>
+                id
+                {id}
+              </p>
+              <div className="description">
+                {description}
               </div>
             </div>
+          </div>
 
-            <div className="price Text-Style-4">
-              <div>Price</div>
-              {' '}
-              <div>{price}</div>
-            </div>
+          <div className="price Text-Style-4">
+            <div>Price</div>
+            {' '}
+            <div>{price}</div>
+          </div>
 
-            <div className="web-link">
-              <a href={webLink} target="_blank" rel="noreferrer">Visit our Web</a>
-            </div>
+          <div className="web-link">
+            <a href={webLink} target="_blank" rel="noreferrer">Visit our Web</a>
+          </div>
 
-            <div className="discover-more">
-              <div>Discover more classes </div>
-              <img src={arrowRight} className="arrowRight-icon" alt="arrowRight icon" />
-            </div>
+          <div className="discover-more">
+            <div>Discover more classes </div>
+            <img src={arrowRight} className="arrowRight-icon" alt="arrowRight icon" />
+          </div>
 
-            <img src={discoverIcon} className="discover-icon" alt="discover icon" />
-            
-            
-            
-              <NavLink to="/reserve" className="color-white next-button">
-                <img src={configIcon} className="config-icon" alt="config icon" />
-                <div className="color-white">Reserve</div>
-                <img src={nextIcon} className="next-icon" alt="next icon" />              
-              </NavLink>
-                    
-            
-          </div> 
-         
+          <img src={discoverIcon} className="discover-icon" alt="discover icon" />
 
-          <NavLink to="/lectures" className="color-white back-button">
-             <img src={backIcon} className="back-icon" alt="back icon" />
-          </NavLink>   
+          <NavLink to="/reserve" className="color-white next-button">
+            <img src={configIcon} className="config-icon" alt="config icon" />
+            <div className="color-white">Reserve</div>
+            <img src={nextIcon} className="next-icon" alt="next icon" />
+          </NavLink>
 
-        </div> 
+        </div>
+
+        <NavLink to="/lectures" className="color-white back-button">
+          <img src={backIcon} className="back-icon" alt="back icon" />
+        </NavLink>
+
       </div>
-    
+    </div>
+
   );
 };
 
