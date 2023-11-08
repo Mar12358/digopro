f1e14a0acc5746d52db19a78bd8ac830dad78613
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-11-08 20:58:31 -0300

    Add categories to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :categories, only: [:index, :show, :create, :update]
       resources :pictures, only: [:index, :show, :create, :update]
       resources :products, only: [:index, :show, :create, :update]
       resources :users, only: [:index, :show] do
