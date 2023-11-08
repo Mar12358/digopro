2e8f64d758355729b6cc5b12440ff1ae04cc09a2
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-11-08 19:00:42 -0300

    Fix Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   has_many :carts
 
   validates :name, presence: true, length: { maximum: 25, too_long: '%<count>s characters is the maximum allowed' }
-  validates :shipping_address, presence: true, length: { maximum: 100, too_long: '%<count>s characters is the maximum allowed' }
-  validates :phone_number1, presence: true, length: { maximum: 20, too_long: '%<count>s characters is the maximum allowed' }
+  validates :shipping_address, presence: true,
+                               length: { maximum: 100, too_long: '%<count>s characters is the maximum allowed' }
+  validates :phone_number1, presence: true,
+                            length: { maximum: 20, too_long: '%<count>s characters is the maximum allowed' }
   validates :phone_number2, length: { maximum: 20, too_long: '%<count>s characters is the maximum allowed' }
 end
