5ec0156d524212740840fe105767e6e01f6a9cb3
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-11-08 17:58:24 -0300

    Fix Rubocop

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,20 +8,18 @@ class Product < ApplicationRecord
   validates :image_url, presence: true
   validates :description, presence: true,
                           length: { maximum: 150, too_long: '%<count>s characters is the maximum allowed' }
-  validates :category, presence: true                          
+  validates :category, presence: true
   validates :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :year, presence: true
   validates :year, numericality: { only_integer: true }
   validates :color, presence: true, length: { maximum: 25, too_long: '%<count>s characters is the maximum allowed' }
   validate :year_within_range
-  
-
 
   def year_within_range
     current_year = Time.now.year
-    if year.present? && (year < 1500 || year > current_year)
-      errors.add(:year, "must be between 1500 and #{current_year}")
-    end
+    return unless year.present? && (year < 1500 || year > current_year)
+
+    errors.add(:year, "must be between 1500 and #{current_year}")
   end
 end
