5949cdf71c73d21749afc5f9cabec36a2a7f5031
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-11-08 16:47:16 -0300

    Add associations for products

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,5 +5,5 @@ class Picture < ApplicationRecord
   validates :image_url, presence: true
   validates :description, presence: true,
                           length: { maximum: 150, too_long: '%<count>s characters is the maximum allowed' }
-  validates :color, presence: true
+  validates :color, presence: true, length: { maximum: 25, too_long: '%<count>s characters is the maximum allowed' }
 end
