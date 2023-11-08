1fa55c3fdb26ead58b35d5adb3c68897910b574e
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-11-08 16:22:05 -0300

    Add validations and associations for Pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ApplicationRecord
+  has_many :products
+
+  validates :name, presence: true, length: { maximum: 25, too_long: '%<count>s characters is the maximum allowed' }
+  validates :image_url, presence: true
+  validates :description, presence: true,
+                          length: { maximum: 150, too_long: '%<count>s characters is the maximum allowed' }
+  validates :color, presence: true
 end
